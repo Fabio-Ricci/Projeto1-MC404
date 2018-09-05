e35149b91f0c71e80111d373a3e9c388a8f0cc98
Fabio-Ricci <fabioricci9@gmail.com>
2018-09-05 15:43:49 -0300

    first commit

---
+++ src/processarEntrada.c
@@ -0,0 +1,51 @@
+#include "montador.h"
+#include <stdio.h>
+
+/*
+Exemplo de erros:
+const char* get_error_string (enum errors code) {
+    switch (code) {
+        case ERR_HEXADECIMAL_NAO_VALIDO:
+            return "LEXICO: Numero Hexadecimal Inválido!";
+        case ERRO_ROTULO_JA_DEFINIDO:
+            return "GRAMATICAL: ROTULO JA FOI DEFINIDO!";
+        case ERR_DIRETIVA_NAO_VALIDA:
+            return "LEXICO: Diretiva não válida";
+*/
+
+/*
+    ---- Você Deve implementar esta função para a parte 1.  ----
+    Entrada da função: arquivo de texto lido e seu tamanho
+    Retorna:
+        * 1 caso haja erro na montagem;
+        * 0 caso não haja erro.
+*/
+int ehHexadecimal(char* palavra){
+    return 0;
+}
+
+int ehDiretiva(char* palavra){
+    return 0;
+}
+
+const char* get_error_string (enum errors code) {
+    switch (code) {
+        case ERR_HEXADECIMAL_NAO_VALIDO:
+            return "LEXICO: Numero Hexadecimal Inválido!";
+        case ERR_ROTULO_JA_DEFINIDO:
+            return "GRAMATICAL: ROTULO JA FOI DEFINIDO!";
+        case ERR_DIRETIVA_NAO_VALIDA:
+            return "LEXICO: Diretiva não válida";
+        case ERR_NUMERO_INVALIDO:
+            return "";
+        case ERR_NOME_INVALIDO:
+            return "";
+    }
+}
+
+int processarEntrada(char* entrada, unsigned tamanho)
+{
+
+    /* printf("Você deve implementar esta função para a Parte 1.\n"); */
+    return 0;
+}
