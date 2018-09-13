8391646c2750e1cbde9d22456378464572270fea
Fabio-Ricci <fabioricci9@gmail.com>
2018-09-13 00:05:40 -0300

    verfica palavra

--- src/processarEntrada.c
+++ src/processarEntrada.c
@@ -1,4 +1,6 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 #include "montador.h"
 #include "token.h"
 
@@ -25,7 +27,7 @@ const char* get_error_string (enum errors code) {
 /**
  * Cria um token e coloca no vetor de token
  */
-void criaToken(char *palavra, TipoDoToken tipo, int numLinha){
+void criaToken(char *palavra, TipoDoToken tipo, int numLinha) {
   Token token;
   token.tipo = tipo;
   token.linha = numLinha;
@@ -34,11 +36,105 @@ void criaToken(char *palavra, TipoDoToken tipo, int numLinha){
 }
 
 /**
+ * Métodos para validar palavra
+ * retorna 1 se eh rotulo, diretiva, etc...
+ * retorn 0 se nao eh
+ */
+int ehRotulo(char *palavra) {
+  if (palavra[strlen(palavra) - 1] == ':') {
+    if (palavra[0] == '.') {
+      return 0;  // rótulo inválido comecando com .
+    }
+    return 1;
+  }
+  return 0;
+}
+
+int ehDiretiva(char *palavra) {
+  if (strcmp(palavra, ".org") || strcmp(palavra, ".align") ||
+      strcmp(palavra, ".wfill") || strcmp(palavra, ".set") ||
+      strcmp(palavra, ".word")) {
+    return 1;
+  }
+  return 0;
+}
+
+int ehInstrucao(char *palavra) {
+  char *intrucoes[19] = {"ld",     "ldinv", "ldabs",  "ldmq",  "ldmqmx",
+                         "store",  "jump",  "jumpl",  "jumpr", "add",
+                         "addabs", "sub",   "subabs", "mult",  "div",
+                         "lsh",    "rsh",   "storal", "storar"};
+  int i;
+  for (i = 0; i < 19; i++) {
+    if (strcmp(palavra, intrucoes[i])) {  // verifica se eh alguma das intrucoes
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int ehHexadecimal(char *palavra) {
+  if (strlen(palavra) == 10) {
+    if (palavra[0] == '0' && palavra[1] == 'x') {
+      int i;
+      for (i = 2; i < 10; i++) {
+        if (!isdigit(palavra[i])) {  // verifica se todos os digitos sao numeros
+          return 0;
+        }
+      }
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int ehDecimal(char *palavra) {
+  if (strlen(palavra) == 8) {
+    int i;
+    for (i = 0; i < 8; i++) {
+      if (!isdigit(palavra[i])) {  // verifica se todos os digitos sao numeros
+        return 0;
+      }
+    }
+    return 1;
+  }
+  return 0;
+}
+
+int ehNome(char *palavra) {
+  return 0;
+}
+
+/**
  * Processa uma palavra
  * retorna ERROR_OFFSET (-1000) se palavra invalida
  * retorna 0 se palavra válida
  */
 int processaPalavra(char *palavra, int numLinha) {
+  if (ehRotulo(palavra)) {
+    criaToken(palavra, DefRotulo, numLinha);
+    return 0;
+  }
+  if (ehDiretiva(palavra)) {
+    criaToken(palavra, Diretiva, numLinha);
+    return 0;
+  }
+  if (ehInstrucao(palavra)) {
+    criaToken(palavra, Instrucao, numLinha);
+    return 0;
+  }
+  if (ehHexadecimal(palavra)) {
+    criaToken(palavra, Hexadecimal, numLinha);
+    return 0;
+  }
+  if (ehDecimal(palavra)) {
+    criaToken(palavra, Decimal, numLinha);
+    return 0;
+  }
+  if (ehNome(palavra)) {
+    criaToken(palavra, Nome, numLinha);
+    return 0;
+  }
   return ERROR_OFFSET;
 }
 
