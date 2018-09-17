88403510bd0dbd30e0f5bb10753ba1f8cd749272
Fabio-Ricci <fabioricci9@gmail.com>
2018-09-17 19:25:19 -0300

    verifica erro gramatical inicial

--- src/processarEntrada.c
+++ src/processarEntrada.c
@@ -42,7 +42,7 @@ void criaToken(char *palavra, TipoDoToken tipo, int numLinha) {
  */
 int ehRotulo(char *palavra) {
   if (palavra[strlen(palavra) - 1] == ':') {
-    if (palavra[0] == '.') {
+    if (palavra[0] == '.' || isdigit(palavra[0])) {
       return 0;  // rótulo inválido comecando com .
     }
     return 1;
@@ -101,13 +101,11 @@ int ehDecimal(char *palavra) {
   return 0;
 }
 
-int ehNome(char *palavra) {
-  return 0;
-}
+int ehNome(char *palavra) { return 0; }
 
 /**
  * Processa uma palavra
- * retorna ERROR_OFFSET (-1000) se palavra invalida
+ * retorna numero da linha se palavra invalida
  * retorna 0 se palavra válida
  */
 int processaPalavra(char *palavra, int numLinha) {
@@ -135,7 +133,7 @@ int processaPalavra(char *palavra, int numLinha) {
     criaToken(palavra, Nome, numLinha);
     return 0;
   }
-  return ERROR_OFFSET;
+  return numLinha;
 }
 
 char *eliminaComentarios(char *linha) { return strtok(linha, '#'); }
@@ -143,7 +141,7 @@ char *eliminaComentarios(char *linha) { return strtok(linha, '#'); }
 /**
  * Processa uma linha, palavra por palavra
  *
- * retorna ERROR_OFFSET (-1000) se linha invalida
+ * retorna numero da linha se linha invalida
  * retorna 0 linha válida
  */
 int processaLinha(char *linha, int numLinha) {
@@ -156,7 +154,7 @@ int processaLinha(char *linha, int numLinha) {
     tipoPalavra = processaPalavra(palavra, numLinha);
 
     if (tipoPalavra == ERROR_OFFSET) {
-      return ERROR_OFFSET;
+      return numLinha;
     }
 
     palavra = strtok(NULL, delimit);
@@ -176,6 +174,86 @@ char *toLowerCase(char *entrada) {
 }
 
 /**
+ * Verifica se ha erro lexico
+ * Retorna 0 se tokens válidos
+ * Retorna numero da linha invalida se tokens invalidos
+ */
+int verificaErroGramatical() {
+  int i, numberOfTokens = getNumberOfTokens();
+  Token atual, prox;
+  if (numberOfTokens > 0) {
+    for (i = 0; i < numberOfTokens - 1; i++) {
+      atual = recuperaToken(i);
+      prox = recuperaToken(i + 1);
+      switch (atual.tipo) {
+        case DefRotulo: {
+          if (atual.linha == prox.linha) {
+            if (prox.tipo == DefRotulo) {  // Rotulo apos Rotulo na mesma linha
+              return prox.linha;
+            }
+          }
+          if (prox.tipo == Hexadecimal || prox.tipo == Decimal ||
+              prox.tipo == Nome) {  // Hexadecimal, Decimal ou nome apos Rotulo
+            return prox.linha;
+          }
+          break;
+        }
+        case Diretiva: {
+          if (prox.tipo == DefRotulo || prox.tipo == Diretiva ||
+              prox.tipo == Instrucao) {
+            // Rotulo, Diretiva ou Instrucao apos Diretiva
+            return prox.linha;
+          }
+          break;
+        }
+        case Instrucao: {
+          if (prox.tipo == DefRotulo || prox.tipo == Diretiva ||
+              prox.tipo == Instrucao || prox.tipo == Decimal) {
+            // Rotulo, Diretiva ou Instrucao apos Diretiva
+            return prox.linha;
+          }
+          break;
+        }
+        case Hexadecimal: {
+          if (atual.linha == prox.linha) {
+            return prox.linha;
+          }
+          if (prox.tipo == Decimal || prox.tipo == Hexadecimal ||
+              prox.tipo == Nome) {
+            // Decimal, Hexadecimal ou Nome apos Hexadecimal
+            return prox.linha;
+          }
+          break;
+        }
+        case Decimal: {
+          if (atual.linha == prox.linha) {
+            return prox.linha;
+          }
+          if (prox.tipo == Decimal || prox.tipo == Hexadecimal ||
+              prox.tipo == Nome) {
+            // Decimal, Hexadecimal ou Nome apos Decimal
+            return prox.linha;
+          }
+          break;
+        }
+        case Nome: {
+          if (atual.linha == prox.linha) {
+            return prox.linha;
+          }
+          if (prox.tipo == Decimal || prox.tipo == Hexadecimal ||
+              prox.tipo == Nome) {
+            // Decimal, Hexadecimal ou Nome apos Nome
+            return prox.linha;
+          }
+          break;
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+/**
  * Processa o a entrada linha a linha
  * retorna 0 se entrada válida
  * retorna 1 se entrada inválida
@@ -185,17 +263,23 @@ int processarEntrada(char *entrada, unsigned tamanho) {
   char delimit[] = "\n";
 
   Token token;
-  int numLinha = 1, tipoLinha;
+  int numLinha = 1, erroLexico = 0;
   char *linha = strtok(entrada, delimit);
   while (linha != NULL) {
-    tipoLinha = processaLinha(linha, numLinha);
+    erroLexico = processaLinha(linha, numLinha);
     numLinha++;
 
-    if (tipoLinha == ERROR_OFFSET) {
+    if (erroLexico != 0) {
+      printf("ERRO LEXICO: palavra invalida na linha %d", erroLexico);
       return 1;  // erro
     }
 
     linha = strtok(NULL, delimit);
+  }
+  int erroGramatical = verificaErroGramatical();
+  if (erroGramatical != 0) {
+    printf("ERRO GRAMATICAL: palavra invalida na linha %d", erroGramatical);
+    return 1;
   }
   /* printf("Você deve implementar esta função para a Parte 1.\n"); */
   return 0;
